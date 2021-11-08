243b8beb3495c2a1682681a4bf95f7dd7eda60e6
yoon <yoonemong@naver.com>
2021-11-09 00:18:20 +0900

    loginForm test

--- security1/src/main/java/com/cos/security1/repository/UserRepository.java
+++ security1/src/main/java/com/cos/security1/repository/UserRepository.java
@@ -4,5 +4,6 @@ import com.cos.security1.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 //@Reponsitory가 없어도 Ioc가 됨
-public interface UserRepository extends JpaRepository<User,Integer> {
+public interface UserRepository extends JpaRepository<User,Integer>{
+
 }
