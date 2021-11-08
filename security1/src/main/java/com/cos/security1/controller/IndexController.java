67736fa06536bacc59b6caeea6ebbf43eb749c65
yoon <yoonemong@naver.com>
2021-11-09 00:07:08 +0900

    security loginform

--- security1/src/main/java/com/cos/security1/controller/IndexController.java
+++ security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,11 +1,22 @@
 package com.cos.security1.controller;
 
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @GetMapping({"","/"})
     public String index(){
@@ -30,22 +41,27 @@ public class IndexController {
         return "manager";
     }
 
-    @GetMapping("/login")
-    @ResponseBody
-    public String login(){
-        return "login";
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        return "loginForm";
     }
 
-    @GetMapping("/join")
-    @ResponseBody
-    public String join(){
-        return "join";
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        return "joinForm";
     }
 
-    @GetMapping("/joinProc")
-    @ResponseBody
-    public String joinProc(){
-        return "회원가입 완료됨";
+    @PostMapping("/join")
+    public String join(User user){
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+
+        userRepository.save(user); // 회원가입 잘됨..비밀번호 : 1234 => 시큐리티로 로그인 할 수없음. 이유는 패스워드ㅁ 암호화가 안되어있음
+        return "redirect:/loginForm";
     }
 
 }
---
+++ security1/src/main/java/com/cos/security1/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.cos.security1.repository;
+
+import com.cos.security1.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//@Reponsitory가 없어도 Ioc가 됨
+public interface UserRepository extends JpaRepository<User,Integer> {
+}
