eb7587db42fe5f9b7d2d868767c499be08dc1a02
yoon <yoonemong@naver.com>
2021-11-22 08:25:48 +0900

    toby start

---
+++ tobyProejct1/db.sql
@@ -0,0 +1,10 @@
+create database springbook;
+use springbook;
+
+create table springbook.usersusers(
+                                      id varchar(10) primary key,
+                                      name varchar(20) not null,
+                                      password varchar(10) not null
+)
+
+select * from springbook.users;
