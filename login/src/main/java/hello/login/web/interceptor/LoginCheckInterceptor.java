20973b6d7faec782e1c6301aef36e7faf2bbe697
yoon <yoonemong@naver.com>
2022-04-05 22:36:46 +0900

    interceptor

---
+++ login/src/main/java/hello/login/web/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,45 @@
+package hello.login.web.interceptor;
+
+import hello.login.web.SessionConst;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+public class LoginCheckInterceptor implements HandlerInterceptor {
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+      throws Exception {
+
+    String requestURI = request.getRequestURI();
+
+    log.info("인증 체그 인터셉터 실행 {}",requestURI);
+
+    HttpSession session = request.getSession();
+
+    if(session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null){
+      log.info("미인증 사용자 요청");
+
+      response.sendRedirect("/login?redirectURL="+requestURI);
+      return false;
+    }
+
+    return true;
+  }
+
+  @Override
+  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+      ModelAndView modelAndView) throws Exception {
+    log.info("Login Check postHandle");
+  }
+
+  @Override
+  public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+      Object handler, Exception ex) throws Exception {
+    log.info("login Check after");
+  }
+}
