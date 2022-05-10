37663b270461b02f0b1167badb46ba4145da96ee
yoon <yoonemong@naver.com>
2022-05-10 20:06:11 +0900

    proxy

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
+//프록시의 근본은 클라이언트와 서버 사이에서 중간 역할을 하는게 프록시라는 개념이고
+// 그 프록시를 통해 하려는 일에 따라 패턴이 나뉘게 된다.
+// 접근제어가 목적 : 프록시 패턴
+// 새로운 기능추가가 목적 : 데코레이터 패턴
 //@Import(AppV1Config.class)
 @Import({AppV2Config.class,AppV1Config.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
