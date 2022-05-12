1407c5b942692ec7cd59a61810a9ffa5546772f3
yoon <yoonemong@naver.com>
2022-05-12 20:58:59 +0900

    proxy

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,8 +2,13 @@ package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v2_proxy.ProxyConfig2;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 //프록시의 근본은 클라이언트와 서버 사이에서 중간 역할을 하는게 프록시라는 개념이고
@@ -11,12 +16,18 @@ import org.springframework.context.annotation.Import;
 // 접근제어가 목적 : 프록시 패턴
 // 새로운 기능추가가 목적 : 데코레이터 패턴
 //@Import(AppV1Config.class)
-@Import({AppV2Config.class,AppV1Config.class})
+//@Import({AppV2Config.class,AppV1Config.class})
+@Import(InterfaceProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
+	}
+
+	@Bean
+	public LogTrace logTrace(){
+		return new ThreadLocalLogTrace();
 	}
 
 }
