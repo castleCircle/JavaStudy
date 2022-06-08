0e3328aec051e5fa9a61f2a47ce400109408484a
yoon <yoonemong@naver.com>
2022-06-08 20:20:40 +0900

    proxy internalcall  problem

---
+++ aop/src/main/java/hello/aop/internalcall/CallServiceV2.java
@@ -0,0 +1,32 @@
+package hello.aop.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CallServiceV2 {
+
+//  private final ApplicationContext applicationContext;
+
+  private final ObjectProvider<CallServiceV2> callServiceProvider;
+
+  public CallServiceV2(
+      ObjectProvider<CallServiceV2> callServiceProvider) {
+    this.callServiceProvider = callServiceProvider;
+  }
+
+  public void external(){
+    log.info("external");
+    final CallServiceV2 callService = callServiceProvider.getObject();
+    callService.internal();
+  }
+
+  public void internal(){
+    log.info("call internal");
+  }
+
+}
