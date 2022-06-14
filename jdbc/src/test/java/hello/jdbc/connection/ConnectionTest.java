e86425bb2f73347f7f80e1bf41d913388ded1c2f
yoon <yoonemong@naver.com>
2022-06-14 19:47:46 +0900

    datasource

---
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,41 @@
+package hello.jdbc.connection;
+
+import static hello.jdbc.connection.ConnectionConst.PASSWORD;
+import static hello.jdbc.connection.ConnectionConst.URL;
+import static hello.jdbc.connection.ConnectionConst.USERNAME;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Slf4j
+public class ConnectionTest {
+
+  @Test
+  void driverManager() throws SQLException {
+    final Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    final Connection connection1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+    log.info("connection={},class={}",connection,connection.getClass());
+    log.info("connection1={},class={}",connection1,connection1.getClass());
+  }
+
+  @Test
+  void dataSourceDriverManager() throws SQLException {
+    final DriverManagerDataSource dataSource = new DriverManagerDataSource(URL,
+        USERNAME, PASSWORD);
+    useDataSource(dataSource);
+  }
+
+  private void useDataSource(DataSource dataSource) throws SQLException {
+    final Connection connection = dataSource.getConnection();
+    final Connection connection1 = dataSource.getConnection();
+    log.info("connection={},class={}",connection,connection.getClass());
+    log.info("connection1={},class={}",connection1,connection1.getClass());
+  }
+
+}
