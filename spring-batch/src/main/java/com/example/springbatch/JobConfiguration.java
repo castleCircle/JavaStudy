4ece2042063a57113a1808b14ec544fcd6b50c1c
yoon <yoonemong@naver.com>
2022-06-27 21:04:46 +0900

    job

--- spring-batch/src/main/java/com/example/springbatch/JobConfiguration.java
+++ spring-batch/src/main/java/com/example/springbatch/JobConfiguration.java
@@ -1,7 +1,6 @@
 package com.example.springbatch;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
@@ -10,43 +9,32 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * Job , Step은 bean으로 등록되기 때문에 중복되면 안됨 고유해야함
- * method가 기준이 되지만 .get() 이름또한 중복되면 안됨
- */
-
-@RequiredArgsConstructor
 @Configuration
-@Slf4j
-public class HelloJobConfiguration {
+@RequiredArgsConstructor
+public class JobConfiguration {
 
   private final JobBuilderFactory jobBuilderFactory;
   private final StepBuilderFactory stepBuilderFactory;
 
   @Bean
-  public Job helloJob(){
-    return jobBuilderFactory.get("helloJob")
-        .start(helloStep1())
-        .next(helloStep2())
+  public Job job(){
+    return jobBuilderFactory.get("job")
+        .start(step1())
+        .next(step2())
         .build();
   }
 
   @Bean
-  public Step helloStep1() {
-    return stepBuilderFactory.get("helloStep1")
+  public Step step2() {
+    return stepBuilderFactory.get("step2")
         .tasklet(new Tasklet() {
           @Override
           public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
               throws Exception {
-
-            log.info("========================");
-            log.info(" >> Hello Spring Batch1!!");
-            log.info("========================");
-
+            System.out.println("step2 was executed");
             return RepeatStatus.FINISHED;
           }
         })
@@ -54,20 +42,17 @@ public class HelloJobConfiguration {
   }
 
   @Bean
-  public Step helloStep2() {
-    return stepBuilderFactory.get("helloStep1")
+  public Step step1() {
+    return stepBuilderFactory.get("step1")
         .tasklet(new Tasklet() {
           @Override
           public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
               throws Exception {
-
-            log.info("========================");
-            log.info(" >> Hello Spring Batch2!!");
-            log.info("========================");
-
+            System.out.println("step1 was executed");
             return RepeatStatus.FINISHED;
           }
         })
         .build();
   }
+
 }
