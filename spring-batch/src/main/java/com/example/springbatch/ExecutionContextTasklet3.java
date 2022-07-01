fa9c207c3e2d0c1b1a5a44ba596943127c1f0fe0
yoon <yoonemong@naver.com>
2022-07-01 23:25:45 +0900

    execution context

--- spring-batch/src/main/java/com/example/springbatch/ExecutionContextTasklet3.java
+++ spring-batch/src/main/java/com/example/springbatch/ExecutionContextTasklet3.java
@@ -6,15 +6,21 @@ import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.stereotype.Component;
 
-public class CustomTasklet implements Tasklet {
+@Component
+public class ExecutionContextTasklet3 implements Tasklet {
 
   @Override
   public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
       throws Exception {
+    System.out.println("step3 was executed");
+    final Object name = chunkContext.getStepContext().getStepExecution().getJobExecution()
+        .getExecutionContext().get("name");
 
-    System.out.println("step2 was executed");
+    if(name == null){
+      chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext().put("name","user1");
+      throw new RuntimeException("step3 was failed");
+    }
 
-//    throw new RuntimeException("step2 failed");
     return RepeatStatus.FINISHED;
   }
 }
