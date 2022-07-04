d48dba966e79226f3903ba6ad34ad01dcf26264b
yoon <yoonemong@naver.com>
2022-07-04 19:46:38 +0900

    batch configuration

--- spring-batch/src/main/java/com/example/springbatch/JobConfiguration2.java
+++ spring-batch/src/main/java/com/example/springbatch/JobConfiguration2.java
@@ -2,7 +2,6 @@ package com.example.springbatch;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -15,27 +14,28 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class JobLauncherConfiguration {
+public class JobConfiguration2 {
 
   private final JobBuilderFactory jobBuilderFactory;
   private final StepBuilderFactory stepBuilderFactory;
 
 
   @Bean
-  public Job job() {
-    return jobBuilderFactory.get("job")
-        .start(step1())
-        .next(step2())
+  public Job job2() {
+    return jobBuilderFactory.get("batchJob2")
+        .start(step3())
+        .next(step4())
         .build();
   }
 
   @Bean
-  public Step step1(){
-    return stepBuilderFactory.get("step1")
+  public Step step3(){
+    return stepBuilderFactory.get("step3")
         .tasklet(new Tasklet() {
           @Override
           public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
               throws Exception {
+            System.out.println("step1 was executed");
             return RepeatStatus.FINISHED;
           }
         })
@@ -43,12 +43,13 @@ public class JobLauncherConfiguration {
   }
 
   @Bean
-  public Step step2(){
-    return stepBuilderFactory.get("step2")
+  public Step step4(){
+    return stepBuilderFactory.get("step4")
         .tasklet(new Tasklet() {
           @Override
           public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
               throws Exception {
+            System.out.println("step2 was executed");
             return RepeatStatus.FINISHED;
           }
         })
