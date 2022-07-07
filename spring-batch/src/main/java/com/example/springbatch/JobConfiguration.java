c44434f8868b06e8c3af0d605054bbb4a1ae6806
yoon <yoonemong@naver.com>
2022-07-07 20:44:07 +0900

    step job

--- spring-batch/src/main/java/com/example/springbatch/JobConfiguration.java
+++ spring-batch/src/main/java/com/example/springbatch/JobConfiguration.java
@@ -1,11 +1,19 @@
 package com.example.springbatch;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -22,9 +30,29 @@ public class JobConfiguration {
 
   @Bean
   public Job job() {
-    return jobBuilderFactory.get("batchJob1")
+    return jobBuilderFactory.get("batchJob")
         .start(step1())
         .next(step2())
+        .next(step3())
+        .incrementer(new RunIdIncrementer())
+        .validator(new JobParametersValidator() {
+          @Override
+          public void validate(JobParameters parameters) throws JobParametersInvalidException {
+
+          }
+        })
+        .preventRestart()
+        .listener(new JobExecutionListener() {
+          @Override
+          public void beforeJob(JobExecution jobExecution) {
+
+          }
+
+          @Override
+          public void afterJob(JobExecution jobExecution) {
+
+          }
+        })
         .build();
   }
 
@@ -50,6 +78,22 @@ public class JobConfiguration {
           public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
               throws Exception {
             System.out.println("step2 was executed");
+            return RepeatStatus.FINISHED;
+          }
+        })
+        .build();
+  }
+
+  @Bean
+  public Step step3(){
+    return stepBuilderFactory.get("step3")
+        .tasklet(new Tasklet() {
+          @Override
+          public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
+              throws Exception {
+//            chunkContext.getStepContext().getStepExecution().setStatus(BatchStatus.FAILED);
+//            contribution.setExitStatus(ExitStatus.STOPPED);
+            System.out.println("step3 was executed");
             return RepeatStatus.FINISHED;
           }
         })
