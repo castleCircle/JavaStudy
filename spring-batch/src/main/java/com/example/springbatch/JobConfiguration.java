15fe3c0807df14256fae610a8c706d92804794d1
yoon <yoonemong@naver.com>
2022-07-22 07:39:17 +0900

    flow Job

--- spring-batch/src/main/java/com/example/springbatch/JobConfiguration.java
+++ spring-batch/src/main/java/com/example/springbatch/JobConfiguration.java
@@ -45,12 +45,10 @@ public class JobConfiguration {
   @Bean
   public Job batchJob() {
     return jobBuilderFactory.get("batchJob")
-        .start(step1())
-        .on("COMPLETED")
-        .to(step3())
-        .from(step1())
-        .on("FAILED")
-        .to(step2())
+        .start(flowA())
+        .next(step3())
+        .next(flowB())
+        .next(step6())
         .end()
         .build();
   }
@@ -63,6 +61,26 @@ public class JobConfiguration {
   }
 
   @Bean
+  public Flow flowA(){
+    FlowBuilder<Flow> flowFlowBuilder = new FlowBuilder<>("flowA");
+    flowFlowBuilder.start(step1())
+        .next(step2())
+        .end();
+
+    return flowFlowBuilder.build();
+  }
+
+  @Bean
+  public Flow flowB(){
+    FlowBuilder<Flow> flowFlowBuilder = new FlowBuilder<>("flowB");
+    flowFlowBuilder.start(step4())
+        .next(step5())
+        .end();
+
+    return flowFlowBuilder.build();
+  }
+
+  @Bean
   public Step step1(){
     return stepBuilderFactory.get("step1")
         .tasklet(new Tasklet() {
@@ -70,8 +88,7 @@ public class JobConfiguration {
           public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
               throws Exception {
             System.out.println("step1 was executed");
-            throw new RuntimeException("failed");
-//            return RepeatStatus.FINISHED;
+            return RepeatStatus.FINISHED;
           }
         })
         .build();
@@ -102,7 +119,50 @@ public class JobConfiguration {
             return RepeatStatus.FINISHED;
           }
         })
-        .startLimit(3)
+        .build();
+  }
+
+  @Bean
+  public Step step4(){
+    return stepBuilderFactory.get("step4")
+        .tasklet(new Tasklet() {
+          @Override
+          public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
+              throws Exception {
+            System.out.println("step4 was executed");
+            throw new RuntimeException("step4 failed");
+//            return RepeatStatus.FINISHED;
+          }
+        })
+        .build();
+  }
+
+  @Bean
+  public Step step5(){
+    return stepBuilderFactory.get("step5")
+        .tasklet(new Tasklet() {
+          @Override
+          public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
+              throws Exception {
+            System.out.println("step5 was executed");
+            return RepeatStatus.FINISHED;
+          }
+        })
+        .build();
+  }
+
+
+  @Bean
+  public Step step6(){
+    return stepBuilderFactory.get("step6")
+        .tasklet(new Tasklet() {
+          @Override
+          public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
+              throws Exception {
+            System.out.println("step6 was executed");
+            return RepeatStatus.FINISHED;
+          }
+        })
         .build();
   }
 
