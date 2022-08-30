98a63d8bf8904eb37000eece7c245e7f4e591e49
yoon <yoonemong@naver.com>
2022-08-30 23:00:07 +0900

    Collection

--- kotlinStudy/src/main/kotlin/0829/1_Collection.kt
+++ kotlinStudy/src/main/kotlin/0829/1_Collection.kt
@@ -61,6 +61,39 @@ fun main(){
         println(it)
     }
 
-    
+    val lowerList = listOf("a","b","c")
+    val upperList = mutableListOf<String>()
+
+    for(lower in lowerList){
+        upperList.add(lower.uppercase())
+    }
+
+    println(upperList)
+
+    val upperList1= lowerList.map {
+        it.uppercase()
+    }
+
+    println(upperList1)
+
+    val filteredList = upperList.filter { it == "A" || it == "C" }
+    println(filteredList)
+
+    val filteredList1 = upperList.filter { it == "A" || it == "C" }
+    println(filteredList1)
+
+
+    //인라인함수들은 일반적으로 filter같은것을 할때마다 컬렉션을 만든다. out of memory 가능성이 생김
+    val tfilterdList = upperList.filter { it == "A" || it == "C" }.toList()
+
+
+    //asSEquence가 좋다.
+    val tfilterdList1 = upperList.asSequence().filter { it == "A" || it == "C" }.toList()
+
+    println(tfilterdList)
+    println(tfilterdList1)
+
+
+
 
 }
