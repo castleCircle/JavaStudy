0280c7d86223cd1ea3ac8fd8e4c2a3ea247758a7
yoon <yoonemong@naver.com>
2023-04-13 12:12:36 +0900

    QueryDslPredicateExecutor

--- querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -13,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 @SpringBootTest
@@ -135,6 +136,40 @@ public class MemberRepositoryTest {
 
     assertThat(result.getContent().size()).isEqualTo(4);
 //    assertThat(result.getContent()).extracting("username").containsExactly("member1","member2","member3");
+
+  }
+
+  /**
+   * QuerydslPredicateExecutor 을 사용하면 쿼리를 좀 편하게 사용할수 있지만
+   * 좀만 더 들어가면 결과적으로는 dto나 이런걸 만들어서 repository로 넘겨서 작업해야해서
+   * 사실 실무에서는 쓰기 힘들다
+   */
+  @Test
+  public void query(){
+
+    Team teamA = new Team("teamA");
+    Team teamB = new Team("teamB");
+    em.persist(teamA);
+    em.persist(teamB);
+
+    Member member1 = new Member("member1",10,teamA);
+    Member member2 = new Member("member2",20,teamA);
+
+    Member member3 = new Member("member3",30,teamB);
+    Member member4 = new Member("member4",40,teamB);
+
+    em.persist(member1);
+    em.persist(member2);
+    em.persist(member3);
+    em.persist(member4);
+
+    QMember member = QMember.member;
+    final Iterable<Member> members = memberRepository.findAll(member.age
+        .between(10, 40).and(member.username.eq("member1")));
+
+    for(Member tempMember : members){
+      System.out.println("members = " + tempMember);
+    }
 
   }
 
