a004b11922ab78f4d7e3c174ea33c70c5ba94ee1
yoon <yoonemong@naver.com>
2024-01-09 08:33:48 +0900

    thread interruted

---
+++ MultiThreadTutorial/src/main/java/wony20240109/InterrutedExceptionExample.java
@@ -0,0 +1,33 @@
+package wony20240109;
+
+public class InterrutedExceptionExample {
+
+  public static void main(String[] args) throws InterruptedException {
+
+    Thread thread = new Thread(()->{
+      System.out.println("인터럽트 상태 1:" + Thread.currentThread().isInterrupted());
+      try{
+        Thread.sleep(5000);
+      }catch (InterruptedException e){
+        System.out.println("스레드가 인터럽트 되었습니다.");
+        System.out.println("인터럽트 상태 2: " + Thread.currentThread().isInterrupted());
+        Thread.currentThread().interrupt();
+      }
+    });
+
+    thread.start();
+
+    try{
+      Thread.sleep(2000);
+    }catch (InterruptedException e){
+      e.printStackTrace();
+    }
+
+
+    thread.interrupt();
+    thread.join();
+    System.out.println("인터럽트 상태3: " + thread.isInterrupted());
+
+  }
+
+}
