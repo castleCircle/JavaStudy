ecc8cdd91ceab9cdf1426621987783613851d24f
yoon <yoonemong@naver.com>
2024-09-08 10:51:41 +0900

    synchronized sample

---
+++ javaAdv1/src/thread/control/sync/BankAccountV1.java
@@ -0,0 +1,39 @@
+package thread.control.sync;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BankAccountV1 implements BankAccount{
+
+  private int balance;
+
+  public BankAccountV1(int balance) {
+    this.balance = balance;
+  }
+
+  @Override
+  public boolean withdraw(int amount) {
+    log("거래 시작: " + getClass().getSimpleName());
+    //잔고가 출금액 보다 적으면, 진행하면 안됨
+
+    log("[검증시작] 출금액: " + amount + ", 잔액: " + balance);
+    if(balance < amount){
+      log("[검증실패] 출금액: " + amount + ", 잔액:" + balance);
+      return false;
+    }
+
+    //잔고가 출금액 보다 많으면 진행
+    log("[검증완료] 출금액: " + amount + ", 잔액:" + balance);
+    sleep(1000);
+    balance = balance - amount;
+    log("[출금완] 출금액: " + amount + ", 잔액:" + balance);
+
+    log("거래 종료: " + getClass().getSimpleName());
+    return true;
+  }
+
+  @Override
+  public int getBalance() {
+    return balance;
+  }
+}
