88947422026f7a62d411f1727554f5e5b1c12af5
yoon <yoonemong@naver.com>
2024-09-10 08:15:21 +0900

    reentrantLock

---
+++ javaAdv1/src/thread/control/sync/BankAccountV4.java
@@ -0,0 +1,55 @@
+package thread.control.sync;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BankAccountV4 implements BankAccount{
+
+  private int balance;
+
+  private final Lock lock = new ReentrantLock();
+
+  public BankAccountV4(int balance) {
+    this.balance = balance;
+  }
+
+  @Override
+  public boolean withdraw(int amount) {
+    log("거래 시작: " + getClass().getSimpleName());
+    //잔고가 출금액 보다 적으면, 진행하면 안됨
+
+    //===임계 영역 시작==
+    lock.lock();
+    try {
+      log("[검증시작] 출금액: " + amount + ", 잔액: " + balance);
+      if (balance < amount) {
+        log("[검증실패] 출금액: " + amount + ", 잔액:" + balance);
+        return false;
+      }
+
+      //잔고가 출금액 보다 많으면 진행
+      log("[검증완료] 출금액: " + amount + ", 잔액:" + balance);
+      sleep(1000);
+      balance = balance - amount;
+      log("[출금완] 출금액: " + amount + ", 잔액:" + balance);
+      //===임계 영역 종료==
+    }finally {
+      lock.unlock();
+    }
+    log("거래 종료: " + getClass().getSimpleName());
+    return true;
+  }
+
+  @Override
+  public int getBalance() {
+    lock.lock();
+    try {
+      return balance;
+    }finally {
+      lock.unlock();
+    }
+  }
+}
