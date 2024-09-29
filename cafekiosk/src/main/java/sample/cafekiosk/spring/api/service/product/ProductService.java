218ba5c9d41e588c071bc08d4290ba708893fce4
yoon <yoonemong@naver.com>
2024-09-29 17:57:45 +0900

    presentation start

--- cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -1,19 +1,59 @@
 package sample.cafekiosk.spring.api.service.product;
 
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+import sample.cafekiosk.spring.domain.product.ProductType;
 
+/**
+ * readOnly = true : 읽기전용
+ * CRUD 에서 CUD 동작 X / Only Read
+ * JPA : CUD 스냅샷 저장, 변경감지 X (성능 향상)
+ *
+ * CQRS - COMMAND / READ
+ */
+@Transactional(readOnly = true)
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
   private final ProductRepository productRepository;
+
+  //동시성 이슈 고민해야함
+  @Transactional
+  public ProductResponse createProduct(ProductCreateRequest request) {
+    // productNumber
+    // 001 002 003 004
+    // DB에서 마지막 저장된 Product의 상품 번호를 읽어와서 +1
+    // 009 -> 010
+    final String nextProductNumber = createNextProductNumber();
+
+    Product product = request.toEntity(nextProductNumber);
+    Product savedProduct = productRepository.save(product);
+
+    return ProductResponse.of(savedProduct);
+  }
+
+  private String createNextProductNumber() {
+    String latestProductNumber = productRepository.findLatestProductNumber();
+    if(latestProductNumber == null) {
+      return "001";
+    }
+
+    final int latestProductNumberInt= Integer.parseInt(latestProductNumber);
+    int nextProductNumberInt = latestProductNumberInt + 1;
+    
+    return String.format("%03d",nextProductNumberInt);
+  }
 
   public List<ProductResponse> getSellingProducts(){
     final List<Product> products = productRepository.findAllBySellingStatusIn(
@@ -24,5 +64,6 @@ public class ProductService {
         .collect(Collectors.toList());
 
   }
+
 
 }
--- cafekiosk/src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -1,9 +1,10 @@
 package sample.cafekiosk.spring.domain.product;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.HOLD;
 import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.STOP_SELLING;
+import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
 
 import java.util.List;
 import org.assertj.core.groups.Tuple;
@@ -11,7 +12,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 //@SpringBootTest
@@ -28,15 +28,15 @@ class ProductRepositoryTest {
 
     final Product product1 = Product.builder()
         .productNumber("001")
-        .type(ProductType.HANDMADE)
-        .sellingStatus(ProductSellingStatus.SELLING)
+        .type(HANDMADE)
+        .sellingStatus(SELLING)
         .name("아메리카노")
         .price(4000)
         .build();
 
     final Product product2 = Product.builder()
         .productNumber("002")
-        .type(ProductType.HANDMADE)
+        .type(HANDMADE)
         .sellingStatus(HOLD)
         .name("카페라떼")
         .price(4500)
@@ -44,7 +44,7 @@ class ProductRepositoryTest {
 
     final Product product3 = Product.builder()
         .productNumber("003")
-        .type(ProductType.HANDMADE)
+        .type(HANDMADE)
         .sellingStatus(ProductSellingStatus.STOP_SELLING)
         .name("팥빙수")
         .price(7000)
@@ -54,7 +54,7 @@ class ProductRepositoryTest {
 
     //
     final List<Product> products = productRepository.findAllBySellingStatusIn(
-        List.of(ProductSellingStatus.SELLING, HOLD));
+        List.of(SELLING, HOLD));
 
     assertThat(products).hasSize(2)
         .extracting("productNumber","name","sellingStatus")
@@ -71,15 +71,15 @@ class ProductRepositoryTest {
 
     final Product product1 = Product.builder()
         .productNumber("001")
-        .type(ProductType.HANDMADE)
-        .sellingStatus(ProductSellingStatus.SELLING)
+        .type(HANDMADE)
+        .sellingStatus(SELLING)
         .name("아메리카노")
         .price(4000)
         .build();
 
     final Product product2 = Product.builder()
         .productNumber("002")
-        .type(ProductType.HANDMADE)
+        .type(HANDMADE)
         .sellingStatus(HOLD)
         .name("카페라떼")
         .price(4500)
@@ -87,7 +87,7 @@ class ProductRepositoryTest {
 
     final Product product3 = Product.builder()
         .productNumber("003")
-        .type(ProductType.HANDMADE)
+        .type(HANDMADE)
         .sellingStatus(ProductSellingStatus.STOP_SELLING)
         .name("팥빙수")
         .price(7000)
@@ -106,6 +106,50 @@ class ProductRepositoryTest {
         );
   }
 
+  @DisplayName("가장 마지막으로 저장한 상품의 상품번호를 읽어온다.")
+  @Test
+  void findLatestProductNumber(){
+
+    String targetProductNumber = "003";
+    final Product product1 = createProduct("001",HANDMADE,SELLING,"아메리카노",4000);
+    final Product product2 = createProduct("002",HANDMADE,HOLD,"카페라떼",4500);
+    final Product product3 = createProduct(targetProductNumber,HANDMADE,STOP_SELLING,"팥빙수",7000);
+
+    productRepository.saveAll(List.of(product1,product2,product3));
+
+    //when
+    final String latestProductNumber = productRepository.findLatestProductNumber();
+
+    //then
+    assertThat(latestProductNumber).isEqualTo(targetProductNumber);
+  }
+
+  @DisplayName("가장 마지막으로 저장한 상품의 상품번호를 읽어올 때, 상품이 하나도 없는 경우에는 null을 반환한다.")
+  @Test
+  void findLatestProductNumberWhenProductIsEmpty(){
+
+    //when
+    final String latestProductNumber = productRepository.findLatestProductNumber();
+
+    //then
+    assertThat(latestProductNumber).isNull();
+  }
+
+  private Product createProduct(
+      String productNumber,
+      ProductType productType,
+      ProductSellingStatus productSellingStatus,
+      String productName,
+      int price
+  ) {
+    return Product.builder()
+        .productNumber(productNumber)
+        .type(productType)
+        .sellingStatus(productSellingStatus)
+        .name(productName)
+        .price(price)
+        .build();
+  }
 
 
 }
