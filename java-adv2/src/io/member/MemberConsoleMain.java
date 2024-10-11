d84b2673d0e23041f7d300af3d545eb2b088396e
yoon <yoonemong@naver.com>
2024-10-11 23:30:58 +0900

    DataStream

--- java-adv2/src/io/member/MemberConsoleMain.java
+++ java-adv2/src/io/member/MemberConsoleMain.java
@@ -1,14 +1,19 @@
 package io.member;
 
+import io.member.impl.DataMemberRepository;
+import io.member.impl.FileMemberRepository;
 import io.member.impl.MemoryMemberRepository;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
 public class MemberConsoleMain {
 
-  private static final MemberRepository repository = new MemoryMemberRepository();
+//  private static final MemberRepository repository = new MemoryMemberRepository();
+//  private static final MemberRepository repository = new FileMemberRepository();
+  private static final MemberRepository repository = new DataMemberRepository();
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     Scanner scanner = new Scanner(System.in);
     while(true){
       System.out.println("1.회원등록 | 2.회원 목록 조회 | 3.종료");
@@ -35,7 +40,7 @@ public class MemberConsoleMain {
     }
   }
 
-  private static void registerMember(Scanner scanner) {
+  private static void registerMember(Scanner scanner) throws IOException {
     System.out.println("ID 입력:");
     String id = scanner.nextLine();
 
