f47608abaa3ed25b22d8a4e011069e5247288c33
yoon <yoonemong@naver.com>
2024-11-01 07:44:36 +0900

    AutoCloseable exception

---
+++ java-adv2/src/network/tcp/autocloseable/ResourceCloseMainV3.java
@@ -0,0 +1,53 @@
+package network.tcp.autocloseable;
+
+public class ResourceCloseMainV3 {
+
+  /**
+   * 문제는 자원정리하다가 exception 발생한것보다 서비스 로직을 처리하다 exception 발생을 처리하는게 더 중요하다.
+   * 아래와 같은 코드는 서비스 로직처리중 에러가 났는데 fianlly로 가면서 자원정리중 에러가 발생하면서 서비스 예외가 먹혀버린다.
+   */
+  public static void main(String[] args) {
+    try {
+      logic();
+    }catch (CallException e){
+      System.out.println("CallExcpetion 예외 처리");
+      throw new RuntimeException(e);
+    }catch (CloseException e){
+      System.out.println("CloseException 예외 처리");
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static void logic() throws CallException, CloseException {
+    ResourceV1 resource1 = null;
+    ResourceV1 resource2 = null;
+
+    try {
+      resource1 = new ResourceV1("resource1");
+      resource2 = new ResourceV1("resource2");
+
+      resource1.call();
+      resource2.callEx();
+    }catch (CallException e){
+      System.out.println("ex: " + e);
+      throw e;
+    }finally {
+      if(resource2 != null){
+        try {
+          resource2.closeEx(); // CloseException 발생
+        }catch (CloseException e){
+          //close()에서 발생한 예외는 버린다. 필요하면 로깅 정도
+          System.out.println("close ex: " + e);
+        }
+      }
+      if(resource1 != null){
+        try {
+          resource1.closeEx(); // 이 코드 호출 안댐
+        }catch (CloseException e){
+          System.out.println("close ex: " + e);
+        }
+      }
+    }
+  }
+
+}
