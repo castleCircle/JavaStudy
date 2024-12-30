8fe39d5854eb84363316da70491686ddf5400d83
yoon <yoonemong@naver.com>
2024-12-30 23:44:29 +0900

    coroutine:]

---
+++ coroutinelecture/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+}
+
+group = "sample"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
